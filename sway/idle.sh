b0d40aaf89f6b824e5e906905987ceda1fe49775
7KIR7 <tkeyrun@gmail.com>
2022-08-28 16:09:35 +0545

    commit

--- sway/idle.sh
+++ sway/idle.sh
@@ -1,7 +1,6 @@
-#!/bin/sh
-
-swayidle -w -d \
-	timeout 100 '~/.config/sway/lock.sh -f --grace 12 --fade-in 10' \
-	timeout 140 'swaymsg "output * dpms off"' \
-	resume 'swaymsg "output * dpms on"; sleep 2; swaymsg "output * enable"' \
-	before-sleep 'pgrep swaylock || ~/.config/sway/lock.sh -f --fade-in 0 && true'
+#!/bin/env bash
+swayidle \
+	timeout 60 '~/.config/sway/lock.sh --grace 2 --fade-in 4' \
+	timeout 65 'swaymsg "output * dpms off"' \
+	resume 'swaymsg "output * dpms on"' \
+	before-sleep '~/.config/sway/lock.sh'
