4efb4e576721d5d6a49dd26b9c8b5fcc0e7a1bfa
7KIR7 <tkeyrun@gmail.com>
2022-11-26 20:02:19 +0545

    removed unwanted files

--- sway/scripts/battery-low-notify.sh
+++ sway/scripts/battery-low-notify.sh
@@ -13,9 +13,11 @@ while true; do
 			notify-send -i critical -i /usr/share/icons/ePapirus/48x48/status/battery-caution.svg "Battery critical" "$percent% remaining";  mpv .config/sway/scripts/LowBattery.mp3
 			need_notify_critical=0
 			need_notify_low=0
+            sleep 5m
 		elif [ "$need_notify_low" -eq 1 ] && [ "$percent" -eq 30 ]; then
 			notify-send -i /usr/share/icons/ePapirus/48x48/status/battery-low.svg "Battery low" "$percent% remaining"; mpv .config/sway/scripts/LowBattery.mp3
 			need_notify_low=0
+            sleep 5m
 		fi
 	else
 		need_notify_low=1
